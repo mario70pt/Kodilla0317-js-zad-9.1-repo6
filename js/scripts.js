9cedd0295c7c9667bea9cd4a97d91506fca0bd1d
mario70pt <mario70pt@users.noreply.github.com>
2017-04-29 18:14:40 +0200

    first commit II

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,7 @@ console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15)
 console.log(getTriangleArea(10, 15));
-
 var triangle2Area = getTriangleArea(0, 0)
 console.log(getTriangleArea(0, 0));
-
 var triangle3Area = getTriangleArea(20, 35)
 console.log(getTriangleArea(20, 35));
