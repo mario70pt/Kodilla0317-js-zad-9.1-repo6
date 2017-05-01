f1641bc6a010617137f0e85edef70edc99d12613
mario70pt <mario70pt@users.noreply.github.com>
2017-05-01 21:44:40 +0200

    skrócenie kodu js oraz poprawienie wywołania zmiennych

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 function getTriangleArea(a, h) {
     
     if ((a > 0) && (h > 0)) {
-        var triangleArea = a * h / 2;
+        return a * h / 2;
         
     } else {
-    console.log('Nieprawidłowe dane');
+    return 'Nieprawidłowe dane';
     }
-    return triangleArea;
 }
 console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15)
-console.log(getTriangleArea(10, 15));
+console.log(triangle1Area);
 var triangle2Area = getTriangleArea(0, 0)
-console.log(getTriangleArea(0, 0));
+console.log(triangle2Area);
 var triangle3Area = getTriangleArea(20, 35)
-console.log(getTriangleArea(20, 35));
+console.log(triangle3Area);
